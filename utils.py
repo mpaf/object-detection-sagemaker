24dbd6fb9de628c6d74ccfa2f4dc19874b94149b
Miguel Ferreira <mpaf98@gmail.com>
2022-01-11 11:05:19 +0000

    add deployment notebook

--- utils.py
+++ utils.py
@@ -159,3 +159,65 @@ def gen_list_files(size_file, bbox_file, image_file, label_file,
     print("num train: " + str(train_df.shape[0]))
     print("num val: " + str(val_df.shape[0]))
     return train_df, val_df
+
+def visualize_detection(img_file, dets, classes=[], thresh=0.6):
+    """
+    visualize detections in one image
+    Parameters:
+    ----------
+    img : numpy.array
+        image, in bgr format
+    dets : numpy.array
+        ssd detections, numpy.array([[id, score, x1, y1, x2, y2]...])
+        each row is one object
+    classes : tuple or list of str
+        class names
+    thresh : float
+        score threshold
+    """
+    import random
+    import matplotlib.pyplot as plt
+    import matplotlib.image as mpimg
+
+    img = mpimg.imread(img_file)
+    plt.imshow(img)
+    height = img.shape[0]
+    width = img.shape[1]
+    colors = dict()
+    num_detections = 0
+    for det in dets:
+        (klass, score, x0, y0, x1, y1) = det
+        if score < thresh:
+            continue
+        num_detections += 1
+        cls_id = int(klass)
+        if cls_id not in colors:
+            colors[cls_id] = (random.random(), random.random(), random.random())
+        xmin = int(x0 * width)
+        ymin = int(y0 * height)
+        xmax = int(x1 * width)
+        ymax = int(y1 * height)
+        rect = plt.Rectangle(
+            (xmin, ymin),
+            xmax - xmin,
+            ymax - ymin,
+            fill=False,
+            edgecolor=colors[cls_id],
+            linewidth=3.5,
+        )
+        plt.gca().add_patch(rect)
+        class_name = str(cls_id)
+        if classes and len(classes) > cls_id:
+            class_name = classes[cls_id]
+        print("{},{}".format(class_name, score))
+        plt.gca().text(
+            xmin,
+            ymin - 2,
+            "{:s} {:.3f}".format(class_name, score),
+            bbox=dict(facecolor=colors[cls_id], alpha=0.5),
+            fontsize=12,
+            color="white",
+        )
+
+    print("Number of detections: " + str(num_detections))
+    plt.show()
